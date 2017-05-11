8026ad5fc23765421202f1690e50288038588ee5
QuanPV <doraemonfanclub@gmail.com>
2017-05-11 15:57:11 +0700

    Minor fix

--- app/stores/custom-store.js
+++ app/stores/custom-store.js
@@ -3,15 +3,19 @@
  */
 'use strict';
 
-import {createStore, applyMiddleware, compose} from "redux";
-import createSagaMiddleware from "redux-saga";
-import rootReducer from "../reducers";
-import rootSaga from "../sagas";
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from '../reducers';
+import rootSaga from '../sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 
 export default function CustomStore(initialState) {
-  const store = createStore(rootReducer, initialState, compose(applyMiddleware(sagaMiddleware)));
+  const store = createStore(
+    rootReducer,
+    initialState,
+    compose(applyMiddleware(sagaMiddleware)),
+  );
   sagaMiddleware.run(rootSaga);
   return store;
 }
